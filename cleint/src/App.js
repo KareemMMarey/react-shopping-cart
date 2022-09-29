ef8c264b26926fd4cf208962a84b14183ac0575a
KareemMMarey <kareem.m.marey@gmail.com>
2022-09-29 21:35:14 +0300

    main push

---
+++ cleint/src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header/Header";
+
+
+function App() {
+  return (
+      <div className="layout">
+      <Header/>
+      <main>Content</main>
+      <footer>Footer</footer>
+      </div>
+      
+  );
+}
+
+export default App;
