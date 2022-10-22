b393863ed2270d8e056d29a236bef5915d16a56b
KareemMMarey <kareem.m.marey@gmail.com>
2022-10-22 11:25:16 +0300

    redux-filter

--- cleint/src/components/Filter/Filter.js
+++ cleint/src/components/Filter/Filter.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import "../../css/Filter/Filter.css"
 import Flip from 'react-reveal/Flip'
-
+import { connect } from 'react-redux';
+import {filteredSize,filteredSort} from "../../store/actions/products"
+ 
  function Filter(props) {
   return (
     <Flip left>
-        <div className="filter-wrapper">
+        {props.filterProducts&&<div className="filter-wrapper">
         
         <h2 className="filter-title">Filter</h2>
-        <div className="num-of-products">Number of Products {props.productNumber}</div>
+        <div className="num-of-products">Number of Products {props.filterProducts.length}</div>
         <div className='filter-by-size'>
             <span > Filter</span>
-            <select value={props.size} className="filter-select" onChange={props.handleFilterBySiz}>
+            <select value={props.size} className="filter-select" onChange={(e)=>props.filteredSize(props.products,e.target.value)}>
                 <option value="ALL">ALL</option>
                 <option value="XS">XS</option>
                 <option value="S">S</option>
@@ -23,16 +25,23 @@ import Flip from 'react-reveal/Flip'
         </div>
         <div className='filter-by-size'>
             <span > Filter</span>
-            <select value={props.sort} onChange={props.handleFilterByPrice} className="filter-select">
+            <select value={props.sort} onChange={(e)=>props.filteredSort(props.filterProducts,e.target.value)} className="filter-select">
                 <option value="latest">Latest</option>
                 <option value="lowest">Lowest</option>
                 <option value="highest">Heighest</option>
             </select>
         </div>
-    </div>
+    </div>}
     </Flip>
     
   )
 }
 
-export default Filter;
+export default connect((state)=>{
+    return {
+        sort :state.products.sort,
+        size : state.products.size,
+        products:state.products.products,
+        filterProducts: state.products.filterProducts
+    }
+},{filteredSize,filteredSort})(Filter);
