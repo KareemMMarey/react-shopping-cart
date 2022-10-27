08d03d0d57b75d17d7ce3c79ae0f0176c27761a7
KareemMMarey <kareem.m.marey@gmail.com>
2022-10-27 08:06:25 +0300

    router

--- cleint/src/App.js
+++ cleint/src/App.js
@@ -1,30 +1,31 @@
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Product from "./components/Products/Product";
-import Filter from "./components/Filter/Filter";
-import Cart from "./components/Cart/Cart";
 
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 import {Provider} from 'react-redux'
 import store from './store/store'
+import Home from "./pages/Home";
+import Orders from "./pages/Orders";
+import NavBar from "./components/Navbar/NavBar";
 function App() {
   return (
+   <BrowserRouter>
     <Provider store={store}>
       <div className="layout">
       <Header />
       <main>
-        <div className="wrapper">
-          <Product />
-          <Filter 
-           />
-          
-        </div>
-        <Cart/>
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>} exact />
+          <Route path="/orders" element={<Orders/>} exact />
+        </Routes>
       </main>
       <Footer />
     </div>
     </Provider>
+   </BrowserRouter>
 
   );
 }
